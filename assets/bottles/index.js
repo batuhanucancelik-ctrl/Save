793460a76799e08168c7b43b56dbbd9583b769c7
Cera_Seramik_Tozu <batuhanucancelik@gmail.com>
2026-03-12 11:38:47 +0300

    Add files via upload

---
+++ assets/bottles/index.js
@@ -0,0 +1,33 @@
+// assets/bottles/index.js
+// require() çağrıları kendi klasöründeyken parantezli isimleri Metro doğru çözümler
+
+const Bottles = [
+  require('./foto (1).png'),
+  require('./foto (2).png'),
+  require('./foto (3).png'),
+  require('./foto (4).png'),
+  require('./foto (5).png'),
+  require('./foto (6).png'),
+  require('./foto (7).png'),
+  require('./foto (8).png'),
+  require('./foto (9).png'),
+  require('./foto (10).png'),
+  require('./foto (11).png'),
+  require('./foto (12).png'),
+  require('./foto (13).png'),
+  require('./foto (14).png'),
+  require('./foto (15).png'),
+  require('./foto (16).png'),
+  require('./foto (17).png'),
+  require('./foto (18).png'),
+  require('./foto (19).png'),
+  require('./foto (20).png'),
+  require('./foto (21).png'),
+  require('./foto (22).png'),
+  require('./foto (23).png'),
+  require('./foto (24).png'),
+  require('./foto (25).png'),
+  require('./foto (26).png'),
+];
+
+module.exports = Bottles;
